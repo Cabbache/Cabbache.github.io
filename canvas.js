2f6d2b386c5b1213887e7265c3752b2cdc87beb4
Cabbache <lukebjorn@gmail.com>
2018-01-31 14:27:38 +0000

    update

--- canvas.js
+++ canvas.js
@@ -1,7 +1,17 @@
+
+
 function setup() {
   createCanvas(480, 600);
 }
 
+//function draw() {
+//  ellipse(width/2,height/2,100, 100);
+
+//}
+
 function draw() {
-  ellipse(width/2,height/2,100, 100);
+  var max = 20;
+  for (i = 0; i < max*5; i++) {
+    ellipse(50*(Math.sin(i*(max/360)) + 1) + (width/2),(50*(Math.cos(i*(max/360)) + 1)) + height/2,50,50);
+  } 
 }
