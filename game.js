a1ce74ca97d6e48a45720d6b8e8a7b26e3ca2f91
Cabbache <lukescerri@protonmail.com>
2018-02-05 18:55:46 +0100

    Added Nyan food

--- game.js
+++ game.js
@@ -1,4 +1,6 @@
 var player;
+var food;
+var foodImage;
 var playerImage;
 var backgroundImage;
 var playerThrImg;
@@ -10,17 +12,24 @@ var vely = 0;
 var planetImgs = [];
 var planets = [];
 var difficulty = 1;
+var planetcount = 0;
 
 function preload() {
-  planetImgs = [loadImage("p1.png"), loadImage("p2.png"), loadImage("p3.png"), loadImage("p4.png")];
+  planetImgs = [loadImage("p1.png"), loadImage("p2.png"), loadImage("p3.png"), loadImage("p4.png"), loadImage("p5.png")];
   playerImage = loadImage("nyan.png");
   backgroundImage = loadImage("back.jpeg");
   playerThrImg = loadImage("go.png");
+  foodImage = loadImage("food.png");
 }
 
 function setup() {
   createCanvas(screen.width - 30, screen.height - 30);
   player = createSprite(width/2, height/2, 0, 0);
+  food = createSprite(-foodImage.width,-foodImage.height,0,0);
+  food.addImage(foodImage);
+  food.rotationSpeed = 1;
+  food.position.x = (Math.random()*(width - foodImage.width))+foodImage.width;
+  food.position.y = (Math.random()*(height - foodImage.height))+foodImage.height;
 }
 
 function draw() {
@@ -29,24 +38,27 @@ function draw() {
 	player.position.x += velx;
 	player.position.y += vely;
 	rot = player.rotation % 360;
-	if (Math.floor(Math.random()*100) == 5){
+	if (planetcount < difficulty){
 		var indx = Math.floor(Math.random()*planetImgs.length);
 		var img = planetImgs[indx];
 		planets[planets.length] = [createSprite(width + img.width/2, Math.random()*height, 0, 0),-0.1 + (Math.random()*-4), indx];
 		planets[planets.length-1][0].addImage(img);
 		planets[planets.length-1][0].rotationSpeed = (Math.random()*2)-1;
 	}
+	planetcount = 0;
 	for (k = 0;k < planets.length;k++){
 		if (planets[k][0].position.x > -1*(planetImgs[planets[k][2]].width)){
+			planetcount += 1;
 			planets[k][0].position.x += planets[k][1];
 			var radius = planetImgs[planets[k][2]].width/2;
 			var distance = Math.sqrt(Math.pow(player.position.x - planets[k][0].position.x, 2) + Math.pow(player.position.y - planets[k][0].position.y, 2));
 			if (distance < radius){
-				alert("crash");
+				alert("crash. Your score is " + (difficulty-1));
+				window.location.reload();
 			}
 			var mass = Math.PI*Math.pow(radius,2);
-			var cons = 5000;
-			var pull = mass/(cons*(distance+radius));
+			var consG = 5000;
+			var pull = mass/(consG*distance);
 			var angle = Math.atan((player.position.y - planets[k][0].position.y)/(player.position.x - planets[k][0].position.x));
 			var dir = 1;
 			if (player.position.x > planets[k][0].position.x){
@@ -55,6 +67,12 @@ function draw() {
 			vely += pull*Math.sin(angle)*dir;
 			velx += pull*Math.cos(angle)*dir;
 		}
+	}
+	if (player.overlap(food)){
+		difficulty += 1;
+		thrust += 0.05;
+		food.position.x = (Math.random()*(width - foodImage.width))+foodImage.width;
+		food.position.y = (Math.random()*(height - foodImage.height))+foodImage.height;
 	}
 	if (rot > 0){
 		rot = 360-rot;
