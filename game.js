09e5aa82b9c77f7d900e464da79478dd5929c834
Cabbache <lukescerri@protonmail.com>
2018-02-06 20:09:26 +0100

    Added ammo

--- game.js
+++ game.js
@@ -11,8 +11,13 @@ var velx = 0;
 var vely = 0;
 var planetImgs = [];
 var planets = [];
-var difficulty = 1;
+var difficulty = 0;
 var planetcount = 0;
+var ammoImage;
+var ammo;
+var ammoSpeed = 20;
+var ammovelx = 0;
+var ammovely = 0;
 
 function preload() {
   planetImgs = [loadImage("p1.png"), loadImage("p2.png"), loadImage("p3.png"), loadImage("p4.png"), loadImage("p5.png")];
@@ -20,6 +25,7 @@ function preload() {
   backgroundImage = loadImage("back.jpeg");
   playerThrImg = loadImage("go.png");
   foodImage = loadImage("food.png");
+  ammoImage = loadImage("ammo.png");
 }
 
 function setup() {
@@ -27,6 +33,8 @@ function setup() {
   player = createSprite(width/2, height/2, 0, 0);
   food = createSprite(-foodImage.width,-foodImage.height,0,0);
   food.addImage(foodImage);
+  ammo = createSprite(-ammoImage.width,-ammoImage.height,0,0);
+  ammo.addImage(ammoImage);
   food.rotationSpeed = 1;
   food.position.x = (Math.random()*(width - foodImage.width))+foodImage.width;
   food.position.y = (Math.random()*(height - foodImage.height))+foodImage.height;
@@ -35,33 +43,38 @@ function setup() {
 function draw() {
     background(backgroundImage);
     drawSprites();
-	player.position.x += velx;
-	player.position.y += vely;
+	motion();
 	rot = player.rotation % 360;
+	if ((ammovelx != 0 || ammovely != 0) && (ammo.position.x < 0 || ammo.position.x > width || ammo.position.y < 0 || ammo.position.y > height)){
+		ammovelx = 0;
+		ammovely = 0;
+	}
 	if (planetcount < difficulty){
-		var indx = Math.floor(Math.random()*planetImgs.length);
-		var img = planetImgs[indx];
-		planets[planets.length] = [createSprite(width + img.width/2, Math.random()*height, 0, 0),-0.1 + (Math.random()*-4), indx];
-		planets[planets.length-1][0].addImage(img);
-		planets[planets.length-1][0].rotationSpeed = (Math.random()*2)-1;
+		summonPlanet();	
 	}
 	planetcount = 0;
 	for (k = 0;k < planets.length;k++){
-		if (planets[k][0].position.x > -1*(planetImgs[planets[k][2]].width)){
+		var px = planets[k][0].position.x;
+		var py = planets[k][0].position.y;
+		if (px > -1*(planetImgs[planets[k][2]].width)){
 			planetcount += 1;
 			planets[k][0].position.x += planets[k][1];
 			var radius = planetImgs[planets[k][2]].width/2;
-			var distance = Math.sqrt(Math.pow(player.position.x - planets[k][0].position.x, 2) + Math.pow(player.position.y - planets[k][0].position.y, 2));
+			var distance = calcDistance(player.position.x, px, player.position.y, py);
 			if (distance < radius){
-				alert("crash. Your score is " + (difficulty-1));
+				alert("Nyan cat got hurt. Your score is " + difficulty + "\n\nControls:\nUp arrow: move\nLeft arrow: turn counter-clockwise\nRight arrow: turn clockwise\nSpace: shoot\n\nPlay in full screen [F11]");
 				window.location.reload();
 			}
+			var distAmmo = calcDistance(ammo.position.x, px, ammo.position.y, py);
+			if (distAmmo < radius && (ammovelx != 0 || ammovely != 0)){
+				planets[k][0].position.x = -1*(planetImgs[planets[k][2]].width) + 10;
+			}
 			var mass = Math.PI*Math.pow(radius,2);
-			var consG = 5000;
+			var consG = 2500;
 			var pull = mass/(consG*distance);
-			var angle = Math.atan((player.position.y - planets[k][0].position.y)/(player.position.x - planets[k][0].position.x));
+			var angle = Math.atan((player.position.y - py)/(player.position.x - px));
 			var dir = 1;
-			if (player.position.x > planets[k][0].position.x){
+			if (player.position.x > px){
 				dir = -1;
 			}
 			vely += pull*Math.sin(angle)*dir;
@@ -91,8 +104,16 @@ function draw() {
 		velx += thrust*Math.cos((Math.PI*rot)/180.0);
 		player.addImage(playerThrImg);
 	}
-	else{
+	else
+	{
 		player.addImage(playerImage);
+	}
+	if (keyDown(32) && (ammovelx == 0 && ammovely == 0)){
+		ammo.rotation = player.rotation;
+		ammo.position.x = player.position.x;
+		ammo.position.y = player.position.y;
+		ammovelx = ammoSpeed*Math.cos((Math.PI*rot)/180.0) + velx;
+		ammovely = -ammoSpeed*Math.sin((Math.PI*rot)/180.0) + vely;
 	}
 	if (player.position.x + playerImage.width/2 > width || player.position.x - playerImage.width/2 < 0){
 		velx *= -1;
@@ -100,4 +121,23 @@ function draw() {
 	if (player.position.y + playerImage.height/2 > height || player.position.y-playerImage.height/2 < 0){
 		vely *= -1;
 	}
+}
+
+function motion() {
+	player.position.x += velx;
+	player.position.y += vely;
+	ammo.position.x += ammovelx;
+	ammo.position.y += ammovely;
+}
+
+function summonPlanet() {
+	var indx = Math.floor(Math.random()*planetImgs.length);
+	var img = planetImgs[indx];
+	planets[planets.length] = [createSprite(width + img.width/2, Math.random()*height, 0, 0),-0.1 + (Math.random()*-4), indx];
+	planets[planets.length-1][0].addImage(img);
+	planets[planets.length-1][0].rotationSpeed = (Math.random()*2)-1;
+}
+
+function calcDistance(x1, x2, y1, y2){
+	return Math.sqrt(Math.pow(x1-x2, 2) + Math.pow(y1-y2, 2));
 }
