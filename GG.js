c6506a44923433794c510dfa2e7044ff49b2db75
Cabbache <lukescerri@protonmail.com>
2018-03-07 14:39:56 +0100

    test

--- GG.js
+++ GG.js
@@ -12,6 +12,10 @@ var situation = firebase.database().ref();
 var players = [];
 var playersSprite = [];
 var objs = [];
+var weapons = [];
+var weapSprites = [];
+var wcount = 0;
+var wlent = 0;
 var myPname = "p";
 var inPname = 0;
 var speed = 5;
@@ -19,7 +23,7 @@ var speed = 5;
 function setup() {
     createCanvas(screen.width - 30, screen.height - 30);
 	situation.on("child_added", function (point) {Get(point);});
-	situation.on("child_changed", function (point) {players = [];objs = [];Get(point);});
+	situation.on("child_changed", function (point) {players = [];objs = [];weapons = [];Get(point);});
 }
 
 function draw() {
@@ -39,6 +43,37 @@ function draw() {
 			playersSprite[i].position.x = players[i].x;
 			playersSprite[i].position.y = players[i].y;
 			playersSprite[i].rotation = players[i].rot;
+			
+			if (i == inPname){
+				var p = i;
+				var totwep = Twep(p);
+				for (k = 0;k < wcount;k++){
+					var curr = (p*4)+(totwep*3)+(3*k);
+					Update(["Players", myPname, "a"+k+"rot"], weapons[curr]+2);
+					Update(["Players", myPname, "a"+k+"x"], weapons[curr+1]+2);
+					Update(["Players", myPname, "a"+k+"y"], weapons[curr+2]+2);
+				}
+			}
+			if (wlent != weapons.length){
+				while (weapSprites.length > 0){
+					removeSprite(weapSprites.pop());
+				}
+				for (h = 0;h < players.length;h++){
+					for (k = 0;k < players[h].wep;k++){
+						var totwep = Twep(h);
+						var sp = createSprite(weapons[(4*h) + (3*k) + (3*totwep) + 1], weapons[(4*h) + (3*k) + (3*totwep) + 2], 20, 20);
+						sp.rotation = (4*h) + (3*k) + (3*totwep);
+						weapSprites[totwep + k] = sp;
+					}
+				}
+			}
+			for (j = 0;j < players[i].wep;j++){
+				var totwep = Twep(i);
+				weapSprites[totwep + j].position.x = weapons[(i*4)+(totwep*3)+(j*3)+1];
+				weapSprites[totwep + j].position.y = weapons[(i*4)+(totwep*3)+(j*3)+2];
+				weapSprites[totwep + j].rotation = weapons[(i*4)+(totwep*3)+(j*3)];
+			}
+			wlent = weapons.length;
 		}
 	}
 	catch (err){
@@ -52,7 +87,7 @@ function draw() {
 	}
 	if (keyDown(68)){
 		Update(["Players", myPname, "x"], playersSprite[inPname].position.x + speed);
-	}
+}
 	if (keyDown(65)){
 		Update(["Players", myPname, "x"], playersSprite[inPname].position.x - speed);
 	}
@@ -62,11 +97,21 @@ function draw() {
 	if (keyDown(RIGHT_ARROW)){
 		Update(["Players", myPname, "rot"], playersSprite[inPname].rotation%360 + 5);
 	}
+	if (keyDown(32)){
+		Update(["Players", myPname, "a"+wcount+"x"], playersSprite[inPname].position.x);
+		Update(["Players", myPname, "a"+wcount+"y"], playersSprite[inPname].position.y);
+		Update(["Players", myPname, "a"+wcount+"rot"], playersSprite[inPname].rotation);
+		Update(["Players", myPname, "wep"], wcount+1);
+		wcount += 1;
+	}
 }
 
 function Get(data) {
 	data.forEach(function(player) {
 		players.push(player.val());
+		player.forEach(function(wep){
+			weapons.push(wep.val());
+		});
     });
 	objs.push(data.val());
 }
@@ -83,6 +128,7 @@ function Setup() {
 	Update(["Players", myPname, "x"], playersSprite[inPname].position.x);
 	Update(["Players", myPname, "y"], playersSprite[inPname].position.y);
 	Update(["Players", myPname, "rot"], playersSprite[inPname].rotation);
+	Update(["Players", myPname, "wep"], 0);
 }
 
 function Update(dir, files) {
@@ -94,10 +140,10 @@ function Update(dir, files) {
 	return false;
 }
 
-window.onbeforeunload = function(){
-   alert("closing");
+function Twep(now){
+	var totwep = 0;
+	for (i = 0;i < now;i++){
+		totwep += players[i].wep;
+	}
+	return totwep;
 }
-
-window.addEventListener("beforeunload", function(e){
-   alert("aa");
-}, false);
